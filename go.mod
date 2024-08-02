834d150a8caf4b24aa730f1a9d87e6d95d0ed874
Kouya Arashiro <a.kouya2001@gmail.com>
2024-08-03 00:22:58 +0900

    Bug fix val err

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/KouyaArashiro/amazon-whish-list-scraper
 
 go 1.18
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/net v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,49 @@ package main
 import (
     "fmt"
     "log"
+    "net/http"
     "os"
     "github.com/joho/godotenv"
+    "github.com/PuerkitoBio/goquery"
 )
+
+type WishlistItem struct {
+    Title string
+    Price string
+    URL   string
+}
+
+func scrapeWishlist(wishlistID string) ([]WishlistItem, error) {
+    url := fmt.Sprintf("https://www.amazon.co.jp/zn/wishlist/ls/%s", wishlistID)
+
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    doc, err := goquery.NewDocumentFromReader(resp.Body)
+    if err != nil {
+        return nil, err
+    }
+
+    var items []WishlistItem
+    //Implement scraping logic
+    doc.Find("li[data-id]").Each(func(i int, s *goquery.Selection) {
+        title := s.Find("h2").Text()
+        price := s.Find(".a-price-whole").First().Text()
+        url, _ := s.Find("a").Attr("href")
+        
+        item := WishlistItem{
+            Title: title,
+            Price: price,
+            URL:   url,
+        }
+        items = append(items, item)
+    })
+
+    return items, nil
+}
 
 func main() {
     err := godotenv.Load()
@@ -13,5 +53,14 @@ func main() {
         log.Fatal("Error loading .env file")
     }
 
-    whishlistID := os.Getenv("AMAZON_WHISHLIST_ID")
+    wishlistID := os.Getenv("AMAZON_WISHLIST_ID")
+
+    items, err := scrapeWishlist(wishlistID)
+    if err != nil {
+        log.Fatalf("Error scraping whislist: %v", err)
+    }
+
+    for _, item := range items {
+        fmt.Println("Title: %s, Price: %s, url: %s\n", item.Title, item.Price, item.URL)
+    }
 }
