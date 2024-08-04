3f13c0a27105ef1a31d1e4270cb54f4b28a5302a
Kouya Arashiro <a.kouya2001@gmail.com>
2024-08-04 23:45:42 +0900

    Insert scraping result to db

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/KouyaArashiro/amazon-whish-list-scraper
 go 1.18
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/PuerkitoBio/goquery v1.9.2 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/chromedp/cdproto v0.0.0-20240801214329-3f85d328b335 // indirect
 	github.com/chromedp/chromedp v0.10.0 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
     "context"
+    "database/sql"
     "fmt"
     "log"
     "os"
-    //"regexp"
     "time"
 
     "github.com/chromedp/chromedp"
     "github.com/joho/godotenv"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type WishlistItem struct {
@@ -20,7 +21,7 @@ type WishlistItem struct {
 }
 
 func scrapeWishlist(wishlistID string) ([]WishlistItem, error) {
-    log.Println("Starting scrapeWishlist function")
+log.Println("Starting scrapeWishlist function")
 
     opts := append(chromedp.DefaultExecAllocatorOptions[:],
         chromedp.Flag("headless", true),
@@ -121,6 +122,96 @@ func scrollAndScrape(ctx context.Context, items *[]WishlistItem, uniqueItems map
     return nil
 }
 
+func saveToDatabase(items []WishlistItem) error {
+    // データベース接続情報を環境変数から取得
+    dbUser := os.Getenv("DB_USER")
+    dbPass := os.Getenv("DB_PASS")
+    dbName := os.Getenv("DB_NAME")
+    dbHost := os.Getenv("DB_HOST")
+
+    // データベース接続
+    db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbName))
+    if err != nil {
+        return fmt.Errorf("error connecting to database: %v", err)
+    }
+    defer db.Close()
+
+    // 接続テスト
+    err = db.Ping()
+    if err != nil {
+        return fmt.Errorf("error pinging database: %v", err)
+    }
+
+    // トランザクション開始
+    tx, err := db.Begin()
+    if err != nil {
+        return fmt.Errorf("error starting transaction: %v", err)
+    }
+    defer tx.Rollback() // トランザクションが正常にコミットされなかった場合はロールバック
+
+    // 既存のタイトルを取得
+    existingTitles, err := getExistingTitles(tx)
+    if err != nil {
+        return fmt.Errorf("error getting existing titles: %v", err)
+    }
+
+    // プリペアードステートメントの作成
+    stmt, err := tx.Prepare("INSERT INTO wishlist_items(title, price, url, isbn) VALUES(?, ?, ?, ?)")
+    if err != nil {
+        return fmt.Errorf("error preparing statement: %v", err)
+    }
+    defer stmt.Close()
+
+    // データの挿入
+    insertedCount := 0
+    skippedCount := 0
+    for _, item := range items {
+        if _, exists := existingTitles[item.Title]; exists {
+            log.Printf("Skipping duplicate title: %s", item.Title)
+            skippedCount++
+            continue
+        }
+
+        _, err = stmt.Exec(item.Title, item.Price, item.URL, item.ISBN)
+        if err != nil {
+            return fmt.Errorf("error inserting item %s: %v", item.Title, err)
+        }
+        insertedCount++
+    }
+
+    // トランザクションのコミット
+    err = tx.Commit()
+    if err != nil {
+        return fmt.Errorf("error committing transaction: %v", err)
+    }
+
+    log.Printf("Inserted %d new items, skipped %d duplicates", insertedCount, skippedCount)
+    return nil
+}
+
+func getExistingTitles(tx *sql.Tx) (map[string]bool, error) {
+    rows, err := tx.Query("SELECT title FROM wishlist_items")
+    if err != nil {
+        return nil, fmt.Errorf("error querying existing titles: %v", err)
+    }
+    defer rows.Close()
+
+    existingTitles := make(map[string]bool)
+    for rows.Next() {
+        var title string
+        if err := rows.Scan(&title); err != nil {
+            return nil, fmt.Errorf("error scanning title: %v", err)
+        }
+        existingTitles[title] = true
+    }
+
+    if err := rows.Err(); err != nil {
+        return nil, fmt.Errorf("error iterating rows: %v", err)
+    }
+
+    return existingTitles, nil
+}
+
 func main() {
     err := godotenv.Load()
     if err != nil {
@@ -140,6 +231,13 @@ func main() {
     }
 
     log.Printf("Total unique items scraped: %d\n\n", len(items))
+
+    err = saveToDatabase(items)
+    if err != nil {
+        log.Fatalf("Error saving to database: %v", err)
+    }
+
+    log.Println("Data successfully saved to database")
 
     for i, item := range items {
         fmt.Printf("Item %d:\n", i+1)
